291afb03d728a65a4d70caaa50fd3e502ea3ee62
Nikita Averyanov <rac1onal@yandex.ru>
2025-09-15 14:07:32 +0300

    hot-fix: поправить vite config ещё раз

--- vite.config.js
+++ vite.config.js
@@ -1,28 +1,32 @@
 import { fileURLToPath, URL } from 'node:url';
 
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import vueJsx from '@vitejs/plugin-vue-jsx';
 import vueDevTools from 'vite-plugin-vue-devtools';
 
 // https://vite.dev/config/
-export default defineConfig({
-	plugins: [
-		vue(),
-		vueJsx(),
-		vueDevTools(),
-	],
-	base: '/vue-app/',
-	resolve: {
-		alias: {
-			'@': fileURLToPath(new URL('./src', import.meta.url))
+export default defineConfig(({ mode }) => {
+	const env = loadEnv(mode, process.cwd(), '');
+
+	return {
+		plugins: [
+			vue(),
+			vueJsx(),
+			vueDevTools(),
+		],
+		base: env.VITE_BASE || '/',
+		resolve: {
+			alias: {
+				'@': fileURLToPath(new URL('./src', import.meta.url))
+			},
 		},
-	},
-	css: {
-		preprocessorOptions: {
-			scss: {
-				additionalData: `@use "@/assets/styles/global" as *;\n`
+		css: {
+			preprocessorOptions: {
+				scss: {
+					additionalData: `@use "@/assets/styles/global" as *;\n`
+				}
 			}
 		}
-	}
+	};
 });
